72cc3de96a7ab3d1d5dcc600569e38df60f5b26e
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-01 17:03:34 +1200

    create Joke class

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Joke
+{
+    public int Id { get; set; }
+    public string JokeQuestion { get; set; }
+    public string JokeAnswer { get; set; }
+
+    public Joke()
+    {
+
+    }
+}                                                   
+                                                                                                                    
